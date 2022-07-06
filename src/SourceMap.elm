22234d5af91e78b278ef4cf455a2d96f3db51fec
Martin Janiczek <martin@janiczek.cz>
2022-07-06 18:37:52 +0200

    Make it a package

---
+++ src/SourceMap.elm
@@ -0,0 +1,17 @@
+module SourceMap exposing (SourceMap)
+
+import Json.Decode as Decode exposing (Decoder)
+
+
+v3 : ()
+v3 =
+    ()
+
+
+type alias SourceMap =
+    {}
+
+
+v3Decoder : Decoder SourceMap
+v3Decoder =
+    Debug.todo "v3 decoder"
